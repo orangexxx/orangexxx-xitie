00ec2ed8c5bef40b73368abb4f93dbe6dd89056f
orangexxx <xiayangxxx@gmail.com>
2016-04-12 22:14:29 +0800

    use ejs

---
+++ public/js/xitie.js
@@ -0,0 +1,56 @@
+var swiperH = new Swiper('.swiper-container-h', {
+  direction: 'vertical',
+  spaceBetween: 0,
+  onInit: function(swiper) {
+    $('.welcome').show();
+    swiperAnimateCache(swiper);
+    swiperAnimate(swiper);
+  },
+  onSlideChangeEnd: function(swiper) {
+    swiperAnimate(swiper);
+  }
+});
+var swiperV = new Swiper('.swiper-container-v', {
+  pagination: '.swiper-pagination-v',
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  spaceBetween: 0,
+  autoplay: 2000,
+});
+function mp3_play(mp3_url) {
+  var qt_mp3_url = "../mp3/dahuajiao.mp3";
+  if (mp3_url) {
+    $('#playbox audio').get(0).pause();
+    $('#playbox audio').get(0).setAttribute('src', mp3_url);
+    // $('#playbox audio').get(0).setAttribute('loop', false);
+    $('#playbox audio').get(0).load();
+    $('#playbox audio').get(0).play();
+  } else {
+    $('#playbox audio').get(0).setAttribute('src', qt_mp3_url);
+    if ($('#playbox').hasClass('on')) {
+      $('#playbox').attr('class', '');
+      $('#playbox audio').get(0).pause();
+    } else {
+      $('#playbox').attr('class', 'on');
+      $('#playbox audio').get(0).play();
+    }
+  }
+}
+$().ready(function() {
+  $(document).one('touchstart', function(e) {
+    var music = document.getElementById("audio");
+    if (music.paused) {
+      music.play();
+    }
+  });
+  document.addEventListener("WeixinJSBridgeReady", function onBridgeReady() {
+    document.getElementById("audio").play();
+  });
+  $("#overlay_ul").bind("click", function(evt) {
+    if ("UL" == evt.target.nodeName) {
+      this.className = 'overlay_ul';
+      $(this).find('li').removeClass('on');
+    }
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var data = require('../data/data');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('output');
+  res.render('index', data);
 });
 
 module.exports = router;
